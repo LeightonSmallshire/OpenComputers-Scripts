9f7b98ca936e445c9ace53e2f7e0b4b6b106a4b7
Leighton Smallshire <lwsid@hotmail.com>
2021-02-25 04:46:50 +0000

    Create netBoot.lua

---
+++ netBoot.lua
@@ -0,0 +1,16 @@
+-- designed for EEPROM; load a file from web and execute it
+-- requires internet card. You need to do "eeprom.setData(<webUrl>)" yourself.
+local code, inet, eeprom, conn = "",
+component.proxy(component.list('internet')()),
+component.proxy(component.list('eeprom')())
+
+conn = inet.request(eeprom.getData())
+conn.finishConnect()
+assert(conn.response() == 200)
+
+for m in conn.read do
+    code = code .. m
+end
+conn.close()
+
+load(code, 'MAIN', 'tb', _G)()
